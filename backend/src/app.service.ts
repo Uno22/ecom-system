22f0a6fdf026945bf1a045c11a6b60257013f907
Truong La <truong.la@sleek.com>
2025-03-19 11:02:59 +0700

    testing

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log('Hello World!!!');
+    console.log('Hello World!!!!');
     return '<h1>This is simple E-Commercial API</h1>';
   }
 }
